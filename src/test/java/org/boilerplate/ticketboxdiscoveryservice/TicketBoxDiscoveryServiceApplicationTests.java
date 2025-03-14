217fe804f808ac54dcaa627708e6ce861d74516c
PhamCongMinh <minhctthvn@gmail.com>
2025-03-14 14:14:51 +0700

    feat: create discovery server with eureka

---
+++ src/test/java/org/boilerplate/ticketboxdiscoveryservice/TicketBoxDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boilerplate.ticketboxdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBoxDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
